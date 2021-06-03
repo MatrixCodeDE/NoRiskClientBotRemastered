a8753501feb0642472d3118eccc7d9dfaf2bb090
MatrixCodeDE <matr.xonyt@gmail.com>
2021-06-03 18:15:55 +0200

    #rules Listener + advanced stats
    Bot schickt bei #rules X die entsprechende regel
    Erweiterte stats, die aktuell noch auskommentiert sind

--- src/main/kotlin/de/polylymer/commands/implementation/StatsCommand.kt
+++ src/main/kotlin/de/polylymer/commands/implementation/StatsCommand.kt
@@ -14,59 +14,111 @@ import de.polylymer.KordEXT.member
 import de.polylymer.database.MongoManager
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.interaction.followUp
+import dev.kord.core.entity.interaction.string
 
 @KordPreview
 object StatsCommand : SlashCommand(
     name = "stats",
-    description = "Shows your profile-stats"
+    description = "Shows your profile-stats"/*,
+    {
+        string("advanced", "Shows your advanced stats"){
+            required = false
+        }
+    }*/
 ) {
 
     override suspend fun handleCommand(interaction: Interaction) {
         interaction.ackowledgePublic().followUp {
-            embed {
-                val guild: Guild = interaction.kord.getGuild(interaction.data.guildId.value!!)!!
-                val member: Member = guild.getMember(interaction.data.member.value!!.userId)
-                title = "Profile of ${member.asUser().username}"
-                val thumb = EmbedBuilder.Thumbnail()
-                thumb.url = member.asMember().asUser().avatar.url
-                thumbnail = thumb
-                val foot = EmbedBuilder.Footer()
-                foot.icon = guild.getIconUrl(Image.Format.GIF)!!
-                foot.text = guild.name
-                footer = foot
-                color = Color(0, 251, 255)
-                field {
-                    name = "Joined At"
-                    value = member.memberData.joinedAt.split("T")[0].replace("-", " ")
-                }
-                field {
-                    name = "Pending"
-                    value = member.memberData.pending.discordBoolean.toString()
-                }
-                if (!member.memberData.premiumSince.value.isNullOrEmpty()) {
+            val advanced = interaction.command.options["advanced"]?.string()
+
+            /* Beta von den Advanced Commands
+            if(advanced.toLowerCase().contains("advanced")) {
+                embed {
+                    val guild: Guild = interaction.kord.getGuild(interaction.data.guildId.value!!)!!
+                    val member: Member = guild.getMember(interaction.data.member.value!!.userId)
+                    title = "Advanced stats of ${member.asUser().username}"
+                    val thumb = EmbedBuilder.Thumbnail()
+                    thumb.url = member.asMember().asUser().avatar.url
+                    thumbnail = thumb
+                    val foot = EmbedBuilder.Footer()
+                    foot.icon = guild.getIconUrl(Image.Format.GIF)!!
+                    foot.text = guild.name
+                    footer = foot
+                    color = Color(0, 250, 40)
+                    if(!member.displayName.isNullOrEmpty()) {
+                        field {
+                            name = "Nickname"
+                            value = member.displayName.toString()
+                        }
+                    }
+                    field{
+                        name = "ID"
+                        value = member.id.asString
+                    }
+                    if (!member.memberData.premiumSince.value.isNullOrEmpty()) {
+                        field {
+                            name = "Nitro-booster since"
+                            value = member.memberData.premiumSince.value!!.split("T")[0].replace("-", " ")
+                        }
+                    }
+                    field{
+                        name = "Messages"
+                        value = "Coming soon"
+                    }
                     field {
-                        name = "Nitro-booster since"
-                        value = member.memberData.premiumSince.value!!.split("T")[0].replace("-", " ")
+                        val capeOfTheDayChannel = interaction.guild().getChannel(Snowflake("811526154469113886"))
+                        name = "Capes of the Day"
+                        value =
+                            "This user has ${MongoManager.getUserData(interaction.member().id.asString).capesOfTheDay} capes in ${capeOfTheDayChannel.mention}"
                     }
                 }
-                field {
-                    name = "Roles"
-                    var string = ""
-                    for (id in member.memberData.roles) {
-                        string += "@${interaction.guild().getRole(id).name}, "
+            } else {*/
+                embed {
+                    val guild: Guild = interaction.kord.getGuild(interaction.data.guildId.value!!)!!
+                    val member: Member = guild.getMember(interaction.data.member.value!!.userId)
+                    title = "Profile of ${member.asUser().username}"
+                    val thumb = EmbedBuilder.Thumbnail()
+                    thumb.url = member.asMember().asUser().avatar.url
+                    thumbnail = thumb
+                    val foot = EmbedBuilder.Footer()
+                    foot.icon = guild.getIconUrl(Image.Format.GIF)!!
+                    foot.text = guild.name
+                    footer = foot
+                    color = Color(0, 251, 255)
+                    field {
+                        name = "Joined At"
+                        value = member.memberData.joinedAt.split("T")[0].replace("-", " ")
                     }
-                    value = if(interaction.member().asMember().memberData.roles.isEmpty()) {
-                        "No roles"
-                    } else {
-                        string
+                    field {
+                        name = "Pending"
+                        value = member.memberData.pending.discordBoolean.toString()
+                    }
+                    if (!member.memberData.premiumSince.value.isNullOrEmpty()) {
+                        field {
+                            name = "Nitro-booster since"
+                            value = member.memberData.premiumSince.value!!.split("T")[0].replace("-", " ")
+                        }
+                    }
+                    field {
+                        name = "Roles"
+                        var string = ""
+                        for (id in member.memberData.roles) {
+                            string += "@${interaction.guild().getRole(id).name}, "
+                        }
+                        value = if (interaction.member().asMember().memberData.roles.isEmpty()) {
+                            "No roles"
+                        } else {
+                            string
+                        }
+                    }
+                    field {
+                        val capeOfTheDayChannel = interaction.guild().getChannel(Snowflake("811526154469113886"))
+                        name = "Capes of the Day"
+                        value =
+                            "This user has ${MongoManager.getUserData(interaction.member().id.asString).capesOfTheDay} capes in ${capeOfTheDayChannel.mention}"
                     }
                 }
-                field {
-                    val capeOfTheDayChannel = interaction.guild().getChannel(Snowflake("811526154469113886"))
-                    name = "Capes of the Day"
-                    value = "This user has ${MongoManager.getUserData(interaction.member().id.asString).capesOfTheDay} capes in ${capeOfTheDayChannel.mention}"
-                }
-            }
+            //}
         }
     }
 }
--- src/main/kotlin/de/polylymer/listener/MessageListener.kt
+++ src/main/kotlin/de/polylymer/listener/MessageListener.kt
@@ -85,13 +85,96 @@ object MessageListener {
                         }
                     }
 
-                    if (this.message.content.toLowerCase().contains("omegalul")) {
-                        this.message.addReaction(ReactionEmoji.Unicode(Emojis.one.unicode))
-                        this.message.addReaction(ReactionEmoji.Unicode(Emojis.zero.unicode))
+                    if(msg.contains("@!370219437242187788")) {
+                        this.message.reply { content = "Matrix will nicht gepingt werden, daher der Prefix..." }
                     }
 
-                    if(this.message.content.contains("@[NoPing] MatrixGraphicz")) {
-                        this.message.reply { content = "Matrix will nicht gepingt werden, daher der Prefix..." }
+                    //Rules Listener
+                    if (msg.contains("#!774271970741190666")){
+                        if(msg.contains("1.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.grinning.unicode + "1. Seid lieb zu einander, behandelt euch mit Respekt.\n" +
+                                        Emojis.grinning.unicode + "1. Be kind and respectful to others."
+                            )
+                        }
+                        if(msg.contains("2.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.warning.unicode + "2. Anstößige Inhalte sind untersagt\n" +
+                                        Emojis.warning.unicode + "2. NSFW content is not allowed."
+                            )
+                        }
+                        if(msg.contains("3.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.mega.unicode + "3. Werbung ist verboten!\n" +
+                                        Emojis.mega.unicode + "3. Advertisment is forbidden!"
+                            )
+                        }
+                        if(msg.contains("4.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.shield.unicode + "4. Eure Privaten Informationen bleiben eure privaten Informationen!\n" +
+                                        Emojis.shield.unicode + "4. Your private informaton stays your private information!"
+                            )
+                        }
+                        if(msg.contains("5.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.noBell.unicode + "5. Grundlose Pings oder \"Ghost Pings\" sind verboten!\n" +
+                                        Emojis.noBell.unicode + "5. Unnecessary pings and \"ghost pings\" aren't allowed!"
+                            )
+                        }
+                        if(msg.contains("6.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.incomingEnvelope.unicode + "6. Spamming ist untersagt!\n" +
+                                        Emojis.incomingEnvelope.unicode + "6. Spamming is forbidden!"
+                            )
+                        }
+                        if(msg.contains("7.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.link.unicode + "7. Links zu Webseiten / Discord Servern oder anderem sind untersagt!\n" +
+                                        Emojis.link.unicode + "7. Links to other Discord servers or websites that don't have to do something with the NoRisk-Client are forbidden!"
+                            )
+                        }
+                        if(msg.contains("8.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.thinking.unicode + "8. Das Team muss sich zu keinem Zeitpunkt für eine Strafe rechtfertigen.\n" +
+                                        Emojis.thinking.unicode + "8. The team does not have to justify any of it's actions."
+                            )
+                        }
+                        if(msg.contains("9.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.airplane.unicode + "9. Das weiterschicken des Clients ist verboten, solange der Downloadlink offline ist.\n" +
+                                        Emojis.airplane.unicode + "9. Should the client be offline you must NOT send it to others."
+                            )
+                        }
+                        if(msg.contains("10.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.question.unicode + "10. Support-Missbrauch ist verboten und führt zu einem Bann.\n" +
+                                        Emojis.question.unicode + "10. Support-Abuse is forbidden and will be punished with a ban."
+                            )
+                        }
+                        if(msg.contains("11.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.lockWithInkPen.unicode + "11. Nicknames dürfen nicht identisch mit jeglichen Usern sein und keine Ränge beinhalten. (z.B. [Admin] NoRiskk wäre verboten).\n" +
+                                        Emojis.lockWithInkPen.unicode + "11. Nicknames mustn't be identical to the one of others and they also mustn't contain any ranks.. (e.g. [Admin] NoRisk is forbidden)."
+                            )
+                        }
+                        if(msg.contains("12.")){
+                            this.message.channel.createMessage(
+                                "**Rules - Regeln** \n" +
+                                        Emojis.x.unicode + "12. Die Ausrede \"Das wusste ich nicht\" oder \"Das war mein Hund\" zählt hier nicht!\n" +
+                                        Emojis.x.unicode + "12. The apology \"I didn't know that\" or \"My dog did that\" doesn't count!"
+                            )
+                        }
                     }
                 }
             }
